770d25478ae2daddd1efbc917fc6ef41ccffaa8f
srijal30 <srijal30@stuy.edu>
2021-12-21 23:00:59 -0500

    idk

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # Compile-Time-Errors
 Salaj Rijal, Michael Kamela, Ariel Fuchs
 
+## Tests Performed
+We tested 10 trials for 3 data sets (of sizes 1M, 10M and 50M). For each trial we generated a random target and measured the time it would take for search to find said target in the array. In order to make sure that binary search yielded a significant result, we multiplied 
+
 ## Results
-The linear search takes on average several milliseconds to complete. The binary search is completed much faster, to the point that for most data sets measuring milliseconds does not yield significant results. To counteract this, we used a while loop that kept on repeating binary search until the result became significant, and then multiplied how many iterations it took to the result of the linear search. Binary search taking significantly less time is to be expected; looking from a mathematical perspective, the worst case scenarios for the number of guesses needed are given by the functions y=x (for lin search) and y=log_2(x) (for bin search). While the growth of a linear search is, well, linear, the growth of a binary search is represented by a logarithm. Logarithmic functions are very slow to grow, and as a result even the largest data sets take a fraction of the time used on a linear search to perform a binary search.
+Linear search takes on average several milliseconds to complete. The binary search is completed much faster, to the point that for most data sets measuring milliseconds does not yield significant results. To counteract this, we used a while loop that kept on repeating binary search until the result became significant, and then multiplied how many iterations it took to the result of the linear search. Binary search taking significantly less time is to be expected; looking from a mathematical perspective, the worst case scenarios for the number of guesses needed are given by the functions y=x (for lin search) and y=log_2(x) (for bin search). While the growth of a linear search is, well, linear, the growth of a binary search is represented by a logarithm. Logarithmic functions are very slow to grow, and as a result even the largest data sets take a fraction of the time used on a linear search to perform a binary search.
+
 ## Conclusions
 Relative to the size of the dataset, performing a binary search is significantly faster than performing a linear search.
-## Tests Performed
-We performed 10 trials of linear and binary searches on arrays of size 1 M, 10 M, and 50 M. The binary search is completed much faster, to the point that for most data sets measuring milliseconds does not yield significant results. To counteract this, we used a while loop that kept on repeating binary search until the result became significant, and then multiplied how many iterations it took to the result of the linear search.
+
+CREDIT TO TEAM PUNTY FOR MULTIPLER IDEA
+
+CREDIT TO TEAM JAVACOFFE FOR BETTER PRINT STATEMENTS IDEA
+
+CREDIT TO UPTOWN LEAPORD ROCK FOR WORST CASE IDEA (ALTHOUGH WE ALTERED)
--- SearchDriver.java
+++ SearchDriver.java
@@ -1,6 +1,5 @@
 public class SearchDriver{
- //done
-
+  public static int multiplier = -1;
   //to populate array with Integers 0 tru (size-1)
   public static Integer[] makeArray( int size ){
     Integer[] result = new Integer[size];
@@ -13,9 +12,14 @@ public class SearchDriver{
   //conduct a binSearch trial and return time taken
   public static double binTrial( Integer[] dataset, Integer target){
     double start = System.currentTimeMillis();
-    BinSearch.binSearch(dataset, target);
-    double end = System.currentTimeMillis();
-    return end-start;
+    double end = 0;
+    multiplier = 1;
+    while( end-start < 1 ){
+      BinSearch.binSearch(dataset, target);
+      end = System.currentTimeMillis();
+      multiplier++;
+    }
+    return (end-start);
   }
 
   //conduct a linSearch trial and return time taken
@@ -23,136 +27,111 @@ public class SearchDriver{
     double start = System.currentTimeMillis();
     LinSearch.linSearch(dataset, target);
     double end = System.currentTimeMillis();
-    return end-start;
-  }
-
-  //conduct both a linSearch and binSearch trial, and keep on doing this until binSearch becomes significant
-  public static double[] linBinTrial( Integer[] dataset, Integer target){
-    double[] result = new double[2]; //index 0 is lin trial result, index 1 is bin trial result
-    int iterations = 0;
-    double binTrialSum = 0.0;
-
-    while( binTrialSum == 0 ){
-      binTrialSum += binTrial(dataset, target);
-      iterations++;
-    }
-
-    result[0] = linTrial(dataset, target) * iterations;
-    result[1] = binTrialSum;
-
-    return result;
+    return (end-start);
   }
 
   //helper method for printing trials
-  public static void printTrials( double[] results ){
-    //header
-    System.out.println("Trial:  Time(ms):");
-    //find average and print results
-    double avg = 0;
-    for( int i = 0; i < results.length; i++){
-      System.out.println(i + ":\t" + results[i]);
-      avg += results[i];
+  public static double[] printTrial( Integer[] dataset, Integer target ){
+    double binTrial = binTrial(dataset, target);
+    double linTrial = linTrial(dataset, target) * multiplier;
+    System.out.println("\t" + binTrial + "\t" + linTrial + "\t\t" + target );
+
+    return new double[]{linTrial, binTrial}; //0: lintrial, 1: bintrial
+  }
+
+  public static double average( double[] data ){
+    double sum = 0;
+    for( double val: data){
+      sum += val;
     }
-    //print average
-    avg /= results.length;
-    System.out.println("AVG:\t" + avg + "\n");
+    return sum/data.length;
   }
 
   public static void main(String[] args) {
-    int trialNum = 100; //easily modified
+    int trialNum = 10; //10 trials for now but can be changed
+    double[] linData = new double[trialNum], binData = new double[trialNum];
+
+    //RANDOM FIRST TEST TO COUNTERACT FIRST TEST MESS UP
+    Integer[] random = makeArray(100000);
+    BinSearch.binSearch(random, 420);
+    LinSearch.linSearch(random, 420);
 
     /*
     DataSet 0
     Name: mkamela
     Size: 1,000,000
     */
+    System.out.println("\n1,000,000 DATA:\n");
     Integer[] mkamela = makeArray(1_000_000);
-    double[] linData0 = new double[trialNum], binData0 = new double[trialNum];
 
+    //header
+    System.out.println("Trial:\tBin:\tLin:\t\tTarget:");
     for( int i = 0; i < trialNum; i++){
       //generate target
       Integer target = (int) (Math.random() * mkamela.length);
-
-      //Conduct Trial
-      double[] trialResult = linBinTrial(mkamela, target);
-
-      //Linear Search
-      linData0[i] = trialResult[0];
-
-      //Binary Search
-      binData0[i] = trialResult[1];
+      //conduct and print trial
+      System.out.print(i);
+      double result[] = printTrial(mkamela, target);
+      linData[i] = result[0];
+      binData[i] = result[1];
     }
+    //worst case
+    System.out.print("WORST");
+    printTrial(mkamela, mkamela.length-1);
+    //average
+    System.out.print("AVG");
+    System.out.println("\t" + average(binData) + "\t" + average(linData) + "\t\tN/A");
 
     /*
     DataSet 1
     Name: srijal
     Size: 10,000,000
     */
+    System.out.println("\n10,000,000 DATA:\n");
     Integer[] srijal = makeArray(10_000_000);
-    double[] linData1 = new double[trialNum], binData1 = new double[trialNum];
 
+    //header
+    System.out.println("Trial:\tBin:\tLin:\t\tTarget:");
     for( int i = 0; i < trialNum; i++){
       //generate target
       Integer target = (int) (Math.random() * srijal.length);
-
-      //Conduct Trial
-      double[] trialResult = linBinTrial(srijal, target);
-
-      //Linear Search
-      linData1[i] = trialResult[0];
-
-      //Binary Search
-      binData1[i] = trialResult[1];
+      //conduct and print trial
+      System.out.print(i);
+      double result[] = printTrial(srijal, target);
+      linData[i] = result[0];
+      binData[i] = result[1];
     }
+    //worst case
+    System.out.print("WORST");
+    printTrial(srijal, srijal.length-1);
+    //average
+    System.out.print("AVG");
+    System.out.println("\t" + average(binData) + "\t" + average(linData) + "\t\tN/A");
 
     /*
     DataSet 2
     Name: afuchs
     Size: 50,000,000
     */
+    System.out.println("\n50,000,000 DATA:\n");
     Integer[] afuchs = makeArray(50_000_000);
-    double[] linData2 = new double[trialNum], binData2 = new double[trialNum];
-
+    
+    //header
+    System.out.println("Trial:\tBin:\tLin:\t\tTarget:");
     for( int i = 0; i < trialNum; i++){
       //generate target
       Integer target = (int) (Math.random() * afuchs.length);
-
-      //Conduct Trial
-      double[] trialResult = linBinTrial(afuchs, target);
-
-      //Linear Search
-      linData2[i] = trialResult[0];
-
-      //Binary Search
-      binData2[i] = trialResult[1];
+      //conduct and print trial
+      System.out.print(i);
+      double result[] = printTrial(afuchs, target);
+      linData[i] = result[0];
+      binData[i] = result[1];
     }
-
-
-    //Printing of the Results
-    String sep = "-------------------------------------------";
-    System.out.println();
-
-    //mkamela results
-    System.out.println("Results for mkamela (1,000,000): \n");
-    System.out.println("Linear Search: \n");
-    printTrials( linData0 );
-    System.out.println("Binary Search: \n");
-    printTrials( binData0 );
-    System.out.println(sep);
-    //srijal results
-    System.out.println("Results for srijal (10,000,000): \n");
-    System.out.println("Linear Search: \n");
-    printTrials( linData1 );
-    System.out.println("Binary Search: \n");
-    printTrials( binData1 );
-    System.out.println(sep);
-
-    //afuchs results
-    System.out.println("Results for afuchs (50,000,000): \n");
-    System.out.println("Linear Search: \n");
-    printTrials( linData2 );
-    System.out.println("Binary Search: \n");
-    printTrials( binData2 );
-    System.out.println(sep);
+    //worst case
+    System.out.print("WORST");
+    printTrial(afuchs, afuchs.length-1);
+    //average
+    System.out.print("AVG");
+    System.out.println("\t" + average(binData) + "\t" + average(linData) + "\t\tN/A");
   }
-}
+}   
